e49c18c99f8694c53d5caaa32cf3bc6fe171010d
Utku Enes GURSEL <uenesgursel@gmail.com>
2024-07-04 23:24:03 +0300

    change server port

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,6 @@ async fn main() {
         )
         .layer(CorsLayer::permissive());
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
